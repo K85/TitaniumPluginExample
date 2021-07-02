8e6c06404489db93c4b37ef62c28d4532952840b
HakuzosuSAMA <3172906506@qq.com>
2021-07-02 17:47:15 +0800

    Base

--- src/main/java/TitaniumPluginMain.java
+++ src/main/java/TitaniumPluginMain.java
@@ -1,3 +1,24 @@
+import com.github.ocraft.s2client.bot.S2Agent;
+import com.github.ocraft.s2client.protocol.game.Race;
+import com.sakurawald.plugin.PluginBase;
+import com.sakurawald.plugin.PluginManager;
+
 public class TitaniumPluginMain extends PluginBase {
 
+    @Override
+    public String getPluginName() {
+        return "TerranRushBot";
+    }
+
+    @Override
+    public S2Agent generateS2Agent() {
+        return new TerranRushBot();
+    }
+
+    @Override
+    public void beforeLaunch() {
+        if (this.getChooseBotRace() != Race.TERRAN) {
+            throw new RuntimeException("TerranRushBot只支持选择人族！");
+        }
+    }
 }
